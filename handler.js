410cdb79f8bc8494d5b1198f1d582956c7a8743a
Taylor Callsen <gritto2008@gmail.com>
2019-11-16 07:42:58 -0800

    initial commit of basic non-cache lambda expression

---
+++ handler.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports.gpxStats = async event => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    },
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
